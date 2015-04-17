6d93b2fb564f9311908e5f31ed3ee4ad848ef6e6
Junichi Tokuda <tokuda@bwh.harvard.edu>
2015-04-16 21:12:02 -0400

    STYLE: Add header in the code.

--- CircleFit/CircleFit.cxx
+++ CircleFit/CircleFit.cxx
@@ -1,3 +1,17 @@
+/*=========================================================================
+
+  Program:      Circle Fit
+  Language:     C++
+  Contributors: Laurent Chauvin, Junichi Tokuda
+
+  Copyright (c) Brigham and Women's Hospital. All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+  =========================================================================*/
+
 #include <cmath>
 #include <complex>
 #include "itkListSample.h"
